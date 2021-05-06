bc8e553d649841b6f95df76c4f941a745c4cac33
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-05-05 20:02:21 -0500

    rutas para las diferentes endpoints que tiene el server

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const huertoController = require('../controllers/huertoController');
+const cosechaController = require('../controllers/cosechaController');
+const registroController = require('../controllers/registroController');
+const reportsController = require('../controllers/reportsController');
 
 module.exports = function () {
-	/* Insercion en la BD */
-	router.post('/altura', huertoController.insertHeight);
-	router.post('/humedad', huertoController.insertHumidity);
-	router.post('/temperatura', huertoController.insertTemperature);
 
-	router.post('/cosecha', huertoController.insertHarvest);
-	router.post('/registro', huertoController.insertRecord);
+	//CRUD para Cosecha
+	router.get('/cosecha', cosechaController.getAllHarvest);
+	router.get('/cosecha/:id', cosechaController.getHarvestById);
+	router.post('/cosecha', cosechaController.insertHarvest);
+	router.put('/cosecha/:id', cosechaController.updateHarvest);
+	router.delete('/cosecha/:id', cosechaController.deleteHarvestById);
+
+	//insertar Registro diario de la cosecha
+	router.post('/registro/:idCosecha', registroController.insertRecord);
+
+	//reportes
+	router.get('/reporte-registros-diarios/:idCosecha', reportsController.detailOfRecordsPerDay);
+	router.get('/reporte-cosecha/:idCosecha', reportsController.harvestDetail);
 
 	return router;
 };
